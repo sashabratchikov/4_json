e94e126c2a05d5597e6e753d824d6f818bf98307
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2017-10-22 20:31:33 +0300

    added load_data and pretty_print_json functions

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    f = open(filepath, 'r')
+    data = f.read()
+    f.close()
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(json.loads(data), indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(sys.argv[1]))
